51cdb376d67d46692dbd865ce6c127034de2b6ee
ABertashDev <a.bertash.dev@gmail.com>
2023-02-14 00:51:27 +0200

    ignore

--- ToDoApplication/ToDoApplication/obj/Debug/net6.0-windows/NewTaskWindow.g.i.cs
+++ ToDoApplication/ToDoApplication/obj/Debug/net6.0-windows/NewTaskWindow.g.i.cs
@@ -93,7 +93,7 @@ namespace ToDoApplication {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ToDoApplication;V1.0.0.0;component/newtaskwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ToDoApplication;component/newtaskwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\NewTaskWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
